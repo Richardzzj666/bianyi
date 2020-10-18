9000d245ccef8253aecd1da1c251ac0f6d9b16eb
Ricahrzzj666 <1583484408@qq.com>
2020-10-18 18:05:35 +0800

    2

--- First.java
+++ First.java
@@ -129,6 +129,7 @@ public class First {
             for(int i = 0; i < str.length(); i++){
                 if(str.charAt(i) != 0 || i == str.length() - 1){
                     System.out.println("Int(" + str.substring(i) + ")");
+                    break;
                 }
             }
         }
